9027ae3f418059f1b9c2f1da237668eb9c063b96
Sridhara Dasu <dasu@hep.wisc.edu>
2017-08-09 11:39:56 +0200

    Directives in file

--- HT/MakeHT.c
+++ HT/MakeHT.c
@@ -2,15 +2,23 @@
 #include "MakeHT.h"
 
 void MakeHT(uint16_t rgnET[NCrts*NCrds*NRgns], uint16_t hfET[NCrts*NHFRgns], uint16_t HT[1]) {
+#pragma HLS PIPELINE II=6
+#pragma HLS ARRAY_PARTITION variable=rgnET complete dim=0
+#pragma HLS ARRAY_PARTITION variable=hfET complete dim=0
+#pragma HLS ARRAY_PARTITION variable=HT complete dim=0
 	uint16_t rgnHT = 0;
 	uint16_t hfHT = 0;
 iRgn:
 	for(int iRgn = 0; iRgn < NCrts*NCrds*NRgns; iRgn++) {
+#pragma HLS UNROLL
 		if(rgnET[iRgn] > MinETCutForHT) rgnHT += rgnET[iRgn];
+		else rgnHT += 0;
 	}
 iHFRgn:
 	for(int iHFRgn = 0; iHFRgn < NCrts * NHFRgns; iHFRgn++) {
+#pragma HLS UNROLL
 		if(hfET[iHFRgn] > MinHFETCutForHT) hfHT += hfET[iHFRgn];
+		else hfHT += 0;
 	}
 	HT[0] = rgnHT + hfHT;
 }
--- HT/TestMakeHT.c
+++ HT/TestMakeHT.c
@@ -11,47 +11,48 @@
 void WriteLinkMapHT(uint16_t rgnET[NCrts*NCrds*NRgns], uint16_t hfET[NCrts*NHFRgns], uint16_t HT[1]) {
   // This code is to write suitable mapping of inputs to signals in the CTP7_HLS project from Ales
   // Block 1 of User Code
-  for(int iRgn = 0; iRgn < NCrts*NCrds*NRgns; iRgn++) {
+  int iRgn, iHFRgn, link, loBit, hiBit;
+  for(iRgn = 0; iRgn < NCrts*NCrds*NRgns; iRgn++) {
     printf("rgnET_%d : IN STD_LOGIC_VECTOR (15 downto 0);\n", iRgn);
   }
-  for(int iHFRgn = 0; iHFRgn < NCrts * NHFRgns; iHFRgn++) {
+  for(iHFRgn = 0; iHFRgn < NCrts * NHFRgns; iHFRgn++) {
     printf("hfET_%d : IN STD_LOGIC_VECTOR (15 downto 0);\n", iHFRgn);
   }
-  printf("HT_d0 : OUT STD_LOGIC_VECTOR (15 downto 0);\n\n\n");
+  printf("HT_0 : OUT STD_LOGIC_VECTOR (15 downto 0);\n\n\n");
   // Block 2
-  for(int iRgn = 0; iRgn < NCrts*NCrds*NRgns; iRgn++) {
+  for(iRgn = 0; iRgn < NCrts*NCrds*NRgns; iRgn++) {
     printf("signal rgnET_%d : STD_LOGIC_VECTOR(15 DOWNTO 0);\n", iRgn);
   }
-  for(int iHFRgn = 0; iHFRgn < NCrts * NHFRgns; iHFRgn++) {
+  for(iHFRgn = 0; iHFRgn < NCrts * NHFRgns; iHFRgn++) {
     printf("signal hfET_%d : STD_LOGIC_VECTOR(15 DOWNTO 0);\n", iHFRgn);
   }
-  printf("signal HT_d0 : STD_LOGIC_VECTOR (15 downto 0);\n\n\n");
+  printf("signal HT_0 : STD_LOGIC_VECTOR (15 downto 0);\n\n\n");
   // Block 3
-  for(int iRgn = 0; iRgn < NCrts*NCrds*NRgns; iRgn++) {
+  for(iRgn = 0; iRgn < NCrts*NCrds*NRgns; iRgn++) {
     printf("rgnET_%d => rgnET_%d,\n", iRgn, iRgn);
   }
-  for(int iHFRgn = 0; iHFRgn < NCrts * NHFRgns; iHFRgn++) {
+  for(iHFRgn = 0; iHFRgn < NCrts * NHFRgns; iHFRgn++) {
     printf("hfET_%d => hfET_%d,\n", iHFRgn, iHFRgn);
   }
-  printf("HT_d0 => HT_d0,\n\n\n");
+  printf("HT_0 => HT_0,\n\n\n");
   // Block 4
-  for(int iRgn = 0; iRgn < NCrts*NCrds*NRgns; iRgn++) {
+  for(iRgn = 0; iRgn < NCrts*NCrds*NRgns; iRgn++) {
     // Each link can carry 192-bits, or 12x16-bits of data
     // Each iRgn needs 16 bits
-    int link = (iRgn / 12);
-    int loBit = (iRgn % 12) * 16;
-    int hiBit = loBit + 15;
+    link = (iRgn / 12);
+    loBit = (iRgn % 12) * 16;
+    hiBit = loBit + 15;
     printf("rgnET_%d <= s_INPUT_LINK_ARR( %d )(%d downto %d);\n", iRgn, link, hiBit, loBit);
   }
-  for(int iHFRgn = 0; iHFRgn < NCrts * NHFRgns; iHFRgn++) {
+  for(iHFRgn = 0; iHFRgn < NCrts * NHFRgns; iHFRgn++) {
     // Each link can carry 192-bits, or 12x16-bits of data
     // Each iHFRgn needs 16 bits
-    int link = (iHFRgn / 12) + 21;
-    int loBit = (iHFRgn % 12) * 16;
-    int hiBit = loBit + 15;
+    link = (iHFRgn / 12) + 21;
+    loBit = (iHFRgn % 12) * 16;
+    hiBit = loBit + 15;
     printf("hfET_%d <= s_INPUT_LINK_ARR( %d )(%d downto %d);\n", iHFRgn, link, hiBit, loBit);
   }
-  printf("HT_d0 <= s_OUTPUT_LINK_ARR( 0 )(15 downto 0);\n");
+  printf("s_OUTPUT_LINK_ARR( 0 )(15 downto 0) <= HT_0;\n");
 }
 
 int main(int argc, char **argv) {
@@ -96,19 +97,19 @@ int main(int argc, char **argv) {
 
 	// Determine HT using hardware simulation
 
-	uint16_t hlsHT = 0;
-	MakeHT(rgnET, hfET, &hlsHT);
+	uint16_t hlsHT[1] = {0};
+	MakeHT(rgnET, hfET, hlsHT);
 
 	// Compare
 
-	printf("C says: HT = %d; HLS says: HT = %d\n", HT, hlsHT);;
-	if(HT != hlsHT) {
+	printf("C says: HT = %d; HLS says: HT = %d\n", HT, hlsHT[0]);;
+	if(HT != hlsHT[0]) {
 		printf("Test failed\n");
 		return 1;
 	}
 	else printf("Test succeeded\n");
 
-	WriteLinkMapHT(rgnET, hfET, &hlsHT);
+	WriteLinkMapHT(rgnET, hfET, hlsHT);
 
 	return 0;
 
